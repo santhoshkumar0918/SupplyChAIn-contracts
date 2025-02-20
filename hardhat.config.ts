5778f4ceb83a9b9c3107c5f81a3351ce5f39442a
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2025-02-20 20:23:30 +0530

    updated set up

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sonic: {
+      url: "https://rpc.soniclabs.com",
+      accounts: [
+        "47a5059a10528e00cd4355e50c637e7a53f2eba1c2cd9a7491db8a21c34639a3",
+      ],
+    },
+  },
 };
 
 export default config;
