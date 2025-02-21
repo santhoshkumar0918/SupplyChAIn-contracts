43f0503b78a637d3e472b7046069a4e6d53303a7
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2025-02-21 18:08:58 +0530

    updated .env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   networks: {
     sonic: {
-      url: "https://rpc.soniclabs.com",
-      accounts: [
-        "47a5059a10528e00cd4355e50c637e7a53f2eba1c2cd9a7491db8a21c34639a3",
-      ],
+      url: process.env.SONIC_RPC_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
 };
